b20e2e2a483b463e6d9b119ee441f7d66344988f
José Antonio <delosangelesrodriguezjose@gmail.com>
2021-09-10 12:12:48 +0200

    Toma 2

--- Tarea06/src/main/Main.java
+++ Tarea06/src/main/Main.java
@@ -4,6 +4,6 @@ public class Main {
 
 	public static void main(String[] args) {
 		System.out.println("Hello World!");
-		System.out.println("What's up?");
-		System.out.println("Once more");	}
+		System.out.println("What's up?");	
+		}
 }
